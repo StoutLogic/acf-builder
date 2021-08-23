8eb851ac59559518e17c9e6aa02231da7cec8850
Androlax2 <theo.benoit16@gmail.com>
2021-08-23 11:05:45 +0200

    Add modifyField and removeField to GroupBuilder

--- tests/GroupBuilderTest.php
+++ tests/GroupBuilderTest.php
@@ -7,6 +7,7 @@ use StoutLogic\AcfBuilder\GroupBuilder;
 
 class GroupBuilderTest extends \PHPUnit_Framework_TestCase
 {
+
     public function testGroupBuilderCanAddFields()
     {
         $builder = new GroupBuilder('background');
@@ -63,4 +64,73 @@ class GroupBuilderTest extends \PHPUnit_Framework_TestCase
 
         $this->assertArraySubset($expectedConfig, $builder->build());
     }
+
+     public function testModifyGroup()
+    {
+        $subject = new GroupBuilder('test1');
+
+        $subject->addText('text');
+
+        $subject->modifyField('text', ['label' => 'new label']);
+        
+        $this->assertEquals([
+            'key' => 'field_test1',
+            'name' => 'test1',
+            'label' => 'Test1',
+            'type' => 'group',
+            'sub_fields' => [
+                [
+                    'key' => 'field_test1_text',
+                    'type' => 'text',
+                    'name' => 'text',
+                    'label' => 'new label'
+                ]
+            ]
+        ], $subject->build());
+    }
+
+    public function testRemovingGroup()
+    {
+        $subject = new GroupBuilder('test1');
+
+        $subject->addText('text');
+        $subject->addText('text2');
+        $subject->addText('text3');
+
+        $subject->removeField('text2');
+
+        $buildedSubject = $subject->build();
+
+        $this->assertEquals(2, sizeof($buildedSubject['sub_fields']));
+        $this->assertEquals([
+            [
+                'type' => 'text',
+                'name' => 'text',
+                'label' => 'Text',
+                'key' => 'field_test1_text'
+            ],
+            [
+                'type' => 'text',
+                'name' => 'text3',
+                'label' => 'Text3',
+                'key' => 'field_test1_text3'
+            ],
+        ], $buildedSubject['sub_fields']);
+    }
+
+    /**
+     * @expectedException StoutLogic\AcfBuilder\FieldNotFoundException
+     */
+    public function testRemovingGroupNotFound()
+    {
+        $subject = new GroupBuilder('test1');
+
+        $subject->addText('text');
+        $subject->addText('text2');
+        $subject->addText('text3');
+
+        $subject->removeField('text4');
+    }
+
+
 }
