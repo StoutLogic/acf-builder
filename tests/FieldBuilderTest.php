6be2a4941243f7eea2538850c7745e651b46c729
theobenoit <theo.benoit16@gmail.com>
2022-04-09 17:19:34 +0200

    Add unit tests for `->getConfig()` and the `->getType()` methods in the `FieldBuilder`

--- tests/FieldBuilderTest.php
+++ tests/FieldBuilderTest.php
@@ -17,6 +17,25 @@ class FieldBuilderTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('my_field', $subject->getName());
     }
 
+    public function testGetConfig()
+    {
+        $subject = new FieldBuilder('my_field', 'text', [
+            'foo' => 'bar'
+        ]);
+        $this->assertSame([
+            'name' => 'my_field',
+            'label' => 'My Field',
+            'key' => 'field_my_field',
+            'foo' => 'bar'
+        ], $subject->getConfig());
+    }
+
+    public function testGetType()
+    {
+        $subject = new FieldBuilder('my_field', 'text');
+        $this->assertSame('text', $subject->getType());
+    }
+
     public function testBuild()
     {
         $subject = new FieldBuilder('my_field', 'text', ['prepend' => '$']);
